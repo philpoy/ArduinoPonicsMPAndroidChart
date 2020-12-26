efad3fca36da6a5fce28b1951fae03716ea265e2
philpoy <75316293+philpoy@users.noreply.github.com>
2020-12-26 17:24:20 +0800

    Update ScrollingActivity.java

--- app/src/main/java/com/example/arduinoponics/ScrollingActivity.java
+++ app/src/main/java/com/example/arduinoponics/ScrollingActivity.java
@@ -48,11 +48,8 @@ public class ScrollingActivity extends AppCompatActivity implements View.OnClick
     SwitchCompat btswitch;
     TextView date, serialM, graphdescription;
     Button selectDate;
-    ArcGauge GaugeTemp,GaugeHum;
+    ArcGauge GaugeTemp,GaugeHum,GaugeWTemp,GaugeFlowRate,GaugepH;
     LineChart lineChart;
-    LineDataSet aTempLine = new LineDataSet(null,null);
-    LineDataSet humLine = new LineDataSet(null,null);
-    ArrayList<ILineDataSet>iLineDataSets = new ArrayList<>();
     LineData finalGraph;
     /*GraphView graph;
     private LineGraphSeries<DataPoint> aTemp,hum,wTemp,flowRate,pH;
@@ -97,6 +94,9 @@ public class ScrollingActivity extends AppCompatActivity implements View.OnClick
         //inintialize Gauges
         initializeGaugeTemp();
         initializeGaugeHum();
+        initializeGaugeWtemp();
+        initializeGaugeFlowRate();
+        initializeGaugepH();
 
         //HC-05 Connection:
         btswitch.setOnClickListener(this);
@@ -120,6 +120,10 @@ public class ScrollingActivity extends AppCompatActivity implements View.OnClick
                         String splitBy = ","; //split CSV line
                         final String[] myUpdate = appendedMsg.split(splitBy);
 
+                        if (myUpdate[0].equals("T0")){
+                            serialM.append(myUpdate[1]);
+                        }
+
                         if (myUpdate[0].equals("T1")) {//if first separated value equals T1
                             serialM.append(myUpdate[1]);//display 2nd value
                             /* wrap in try catch */
@@ -127,44 +131,9 @@ public class ScrollingActivity extends AppCompatActivity implements View.OnClick
                                 date.setText(String.format("%s %s", myUpdate[2], myUpdate[3]));//display 3rd value and 4th value
                                 GaugeTemp.setValue(Double.parseDouble(myUpdate[4]));
                                 GaugeHum.setValue(Double.parseDouble(myUpdate[5]));
-
-                                ArrayList<Entry> aTemp = new ArrayList<Entry>();
-                                ArrayList<String> time = new ArrayList<>();
-                                aTemp.add(new Entry(Float.parseFloat(myUpdate[4]),aTempLine.getEntryCount()));
-                                aTempLine.setYVals(aTemp);
-                                aTempLine.setLabel("Ambient Temp(C)");
-
-                                ArrayList<Entry> hum = new ArrayList<Entry>();
-                                hum.add(new Entry(Float.parseFloat(myUpdate[5]),humLine.getEntryCount()));
-                                humLine.setYVals(hum);
-                                humLine.setLabel("Humidity(%)");
-
-                                time.add(humLine.getEntryCount()-1,myUpdate[3]);
-
-                                aTempLine.setColor(Color.RED);
-                                aTempLine.setAxisDependency(YAxis.AxisDependency.LEFT);
-                                aTempLine.setLineWidth(3f);
-                                aTempLine.setCubicIntensity(0.2f);
-                                aTempLine.setValueTextSize(20f);
-
-                                humLine.setColor(Color.CYAN);
-                                humLine.setAxisDependency(YAxis.AxisDependency.LEFT);
-                                humLine.setLineWidth(3f);
-                                humLine.setCubicIntensity(0.2f);
-                                humLine.setValueTextSize(20f);
-
-                                iLineDataSets.clear();
-                                iLineDataSets.add(aTempLine);
-                                iLineDataSets.add(humLine);
-                                finalGraph =new LineData(time, iLineDataSets);
-                                //lineChart.clear();
-                                lineChart.setData(finalGraph);
-
-                                lineChart.moveViewToX(aTempLine.getEntryCount());
-                                lineChart.moveViewToX(humLine.getEntryCount());
-                                //finalGraph.notifyDataChanged();
-                                lineChart.notifyDataSetChanged();
-                                lineChart.invalidate();
+                                GaugeWTemp.setValue(Double.parseDouble(myUpdate[6]));
+                                GaugeFlowRate.setValue(Double.parseDouble(myUpdate[7]));
+                                GaugepH.setValue(Double.parseDouble(myUpdate[8]));
 
                             } catch (Exception e) {//catch any errors
                                 Log.e("update", e.toString());
@@ -175,7 +144,7 @@ public class ScrollingActivity extends AppCompatActivity implements View.OnClick
                             }
                         } /* end of type 1 msg if statement */
 
-                        if (myUpdate[0].equals("T2")){ //if type 2 message is received
+                        if (myUpdate[0].equals("T2")){ //if type 2 message is received (t2 is read from sd card of arduino)
                             try {
                                 graphdescription.setText(myUpdate[1]);
                                 double mU3 = Double.parseDouble(myUpdate[3]);
@@ -186,6 +155,9 @@ public class ScrollingActivity extends AppCompatActivity implements View.OnClick
 
                                 serialM.append("Time: "+myUpdate[2]+", Ambient Temp(C): "+mU3+", Humidity: "+mU4+"%"+
                                         ", Water Temp(C): "+mU5+", Flow Rate(L/min): "+mU6+", pH: "+mU7+"\n");
+
+                                //addToChart(myUpdate[2],myUpdate[3],myUpdate[4]); --->> this line is not working. pseudo code
+                                //...data should be used for graphing here...
 
                             }catch (Exception e){
                                 Log.e("graph error:", e.toString());
@@ -200,6 +172,47 @@ public class ScrollingActivity extends AppCompatActivity implements View.OnClick
 
     }
 
+
+
+    private void addToChart(String s, String s1, String s2) {// not working for line graph
+        ArrayList<Entry> aTemp = new ArrayList<Entry>();
+        for (int i=0; i<=aTemp.size();i++){
+        aTemp.add(new Entry(i, (int) Float.parseFloat(s1)));}
+        ArrayList<Entry> hum = new ArrayList<Entry>();
+        for (int i=0; i<=hum.size();i++){
+        hum.add(new Entry(i, (int) Float.parseFloat(s2)));}
+        ArrayList<String> time = new ArrayList<>();
+        for (int i=0; i<=time.size();i++){
+        time.add(i,s);}
+
+        //add array list entry to line data set
+        LineDataSet aTempLine,humLine,set3;
+        aTempLine =new LineDataSet(aTemp,"Ambient Temperature");
+        humLine =new LineDataSet(hum,"Humidity");
+
+        //add line data sets to iLine Data sets. This handles multiple line datasets.
+        ArrayList<ILineDataSet> sets =new ArrayList<>();
+        sets.add(aTempLine);
+        sets.add(humLine);
+
+        finalGraph =new LineData(time,sets);
+
+        aTempLine.setColor(Color.RED);
+        aTempLine.setAxisDependency(YAxis.AxisDependency.LEFT);
+        aTempLine.setLineWidth(3f);
+        aTempLine.setCubicIntensity(0.2f);
+        aTempLine.setValueTextSize(20f);
+
+        humLine.setColor(Color.CYAN);
+        humLine.setAxisDependency(YAxis.AxisDependency.LEFT);
+        humLine.setLineWidth(3f);
+        humLine.setCubicIntensity(0.2f);
+        humLine.setValueTextSize(20f);
+
+        lineChart.setData(finalGraph);
+        lineChart.notifyDataSetChanged();
+        lineChart.invalidate();
+    }
 
     private void initializeChart() {
         //customize chart
@@ -216,44 +229,24 @@ public class ScrollingActivity extends AppCompatActivity implements View.OnClick
         lineChart.setPinchZoom(true);
     }
 
-    private void initializeGaugeHum() {
-        GaugeHum = findViewById(R.id.GaugeHum);
-        Range rHum1 = new Range();
-        rHum1.setColor(Color.parseColor("#FF0000"));//red
-        rHum1.setFrom(0.0);
-        rHum1.setTo(59.0);
-        Range rHum2 = new Range();
-        rHum2.setColor(Color.parseColor("#FF7F00"));//orange
-        rHum2.setFrom(60.0);
-        rHum2.setTo(79.0);
-        Range rHum3 = new Range();
-        rHum3.setColor(Color.parseColor("#FF00FF00"));//green
-        rHum3.setFrom(80.0);
-        rHum3.setTo(100.0);
-        GaugeHum.addRange(rHum1);
-        GaugeHum.addRange(rHum2);
-        GaugeHum.addRange(rHum3);
-        GaugeHum.setMaxValue(100);
-        GaugeHum.setMinValue(10);
-    }
     private void initializeGaugeTemp() {
         GaugeTemp = findViewById(R.id.GaugeTemp);
         Range rTemp = new Range();
         rTemp.setColor(Color.parseColor("#FF0000"));//red
         rTemp.setFrom(0.0);
-        rTemp.setTo(12.0);
+        rTemp.setTo(15.9);
         Range rTemp2 = new Range();
-        rTemp2.setColor(Color.parseColor("#FF0000"));//orange
-        rTemp2.setFrom(13.0);
-        rTemp2.setTo(16.0);
+        rTemp2.setColor(Color.parseColor("#FF7F00"));//orange
+        rTemp2.setFrom(16.0);
+        rTemp2.setTo(20.9);
         Range rTemp3 = new Range();
         rTemp3.setColor(Color.parseColor("#FF00FF00"));//green
-        rTemp3.setFrom(17.0);
-        rTemp3.setTo(24.0);
+        rTemp3.setFrom(21.0);
+        rTemp3.setTo(25.9);
         Range rTemp4 = new Range();
         rTemp4.setColor(Color.parseColor("#FF7F00"));//orange
-        rTemp4.setFrom(25.0);
-        rTemp4.setTo(29.0);
+        rTemp4.setFrom(26.0);
+        rTemp4.setTo(29.9);
         Range rTemp5 = new Range();
         rTemp5.setColor(Color.parseColor("#FF0000"));//red
         rTemp5.setFrom(30.0);
@@ -264,7 +257,115 @@ public class ScrollingActivity extends AppCompatActivity implements View.OnClick
         GaugeTemp.addRange(rTemp4);
         GaugeTemp.addRange(rTemp5);
         GaugeTemp.setMaxValue(35);
-        GaugeTemp.setMinValue(10);
+        GaugeTemp.setMinValue(0);
+    }
+    private void initializeGaugeHum() {
+        GaugeHum = findViewById(R.id.GaugeHum);
+        Range rHum1 = new Range();
+        rHum1.setColor(Color.parseColor("#FF0000"));//red
+        rHum1.setFrom(0.0);
+        rHum1.setTo(49.9);
+        Range rHum2 = new Range();
+        rHum2.setColor(Color.parseColor("#FF7F00"));//orange
+        rHum2.setFrom(50.0);
+        rHum2.setTo(64.9);
+        Range rHum3 = new Range();
+        rHum3.setColor(Color.parseColor("#FF00FF00"));//green
+        rHum3.setFrom(65.0);
+        rHum3.setTo(85.9);
+        Range rHum4 = new Range();
+        rHum4.setColor(Color.parseColor("#FF7F00"));//orange
+        rHum4.setFrom(86.0);
+        rHum4.setTo(100.0);
+        GaugeHum.addRange(rHum1);
+        GaugeHum.addRange(rHum2);
+        GaugeHum.addRange(rHum3);
+        GaugeHum.addRange(rHum4);
+        GaugeHum.setMaxValue(100);
+        GaugeHum.setMinValue(10);
+    }
+    private void initializeGaugeWtemp() {
+        GaugeWTemp = findViewById(R.id.GaugeWTemp);
+        Range rwTemp = new Range();
+        rwTemp.setColor(Color.parseColor("#FF0000"));//red
+        rwTemp.setFrom(0.0);
+        rwTemp.setTo(16.9);
+        Range rwTemp2 = new Range();
+        rwTemp2.setColor(Color.parseColor("#FF0000"));//orange
+        rwTemp2.setFrom(17.0);
+        rwTemp2.setTo(21.9);
+        Range rwTemp3 = new Range();
+        rwTemp3.setColor(Color.parseColor("#FF00FF00"));//green
+        rwTemp3.setFrom(22.0);
+        rwTemp3.setTo(26.9);
+        Range rwTemp4 = new Range();
+        rwTemp4.setColor(Color.parseColor("#FF7F00"));//orange
+        rwTemp4.setFrom(27.0);
+        rwTemp4.setTo(29.9);
+        Range rwTemp5 = new Range();
+        rwTemp5.setColor(Color.parseColor("#FF0000"));//red
+        rwTemp5.setFrom(30.0);
+        rwTemp5.setTo(35.0);
+        GaugeWTemp.addRange(rwTemp);
+        GaugeWTemp.addRange(rwTemp2);
+        GaugeWTemp.addRange(rwTemp3);
+        GaugeWTemp.addRange(rwTemp4);
+        GaugeWTemp.addRange(rwTemp5);
+        GaugeWTemp.setMaxValue(35);
+        GaugeWTemp.setMinValue(0);
+
+    }
+    private void initializeGaugeFlowRate(){
+        GaugeFlowRate = findViewById(R.id.GaugeFR);
+        Range fr = new Range();
+        fr.setColor(Color.parseColor("#FF0000"));//red
+        fr.setFrom(0.0);
+        fr.setTo(0.9);
+        Range fr2 = new Range();
+        fr2.setColor(Color.parseColor("#FF00FF00"));//green
+        fr2.setFrom(1.0);
+        fr2.setTo(2.9);
+        Range fr3 = new Range();
+        fr3.setColor(Color.parseColor("#FF0000"));//red
+        fr3.setFrom(3.0);
+        fr3.setTo(4.0);
+        GaugeFlowRate.addRange(fr);
+        GaugeFlowRate.addRange(fr2);
+        GaugeFlowRate.addRange(fr3);
+        GaugeFlowRate.setMaxValue(4);
+        GaugeFlowRate.setMinValue(0);
+
+    }
+    private void initializeGaugepH(){
+        GaugepH = findViewById(R.id.GaugepH);
+        Range rph1 = new Range();
+        rph1.setColor(Color.parseColor("#FF0000"));//red
+        rph1.setFrom(0.00);
+        rph1.setTo(5.49);
+        Range rph2 = new Range();
+        rph2.setColor(Color.parseColor("#FF7F00"));//orange
+        rph2.setFrom(5.50);
+        rph2.setTo(5.79);
+        Range rph3 = new Range();
+        rph3.setColor(Color.parseColor("#FF00FF00"));//green
+        rph3.setFrom(5.80);
+        rph3.setTo(6.29);
+        Range rph4 = new Range();
+        rph4.setColor(Color.parseColor("#FF7F00"));//orange
+        rph4.setFrom(6.30);
+        rph4.setTo(6.59);
+        Range rph5 = new Range();
+        rph5.setColor(Color.parseColor("#FF0000"));//red
+        rph5.setFrom(6.60);
+        rph5.setTo(7.59);
+        GaugepH.addRange(rph1);
+        GaugepH.addRange(rph2);
+        GaugepH.addRange(rph3);
+        GaugepH.addRange(rph4);
+        GaugepH.addRange(rph5);
+        GaugepH.setMaxValue(7.5);
+        GaugepH.setMinValue(0);
+
     }
 
     //onClick event
